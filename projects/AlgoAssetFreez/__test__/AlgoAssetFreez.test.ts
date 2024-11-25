02ee6e95514e9ccded3a4e4ca3b33bd63fd0eaf0
Mus'ab <musabyakubu1000@gmail.com>
2024-11-25 16:32:24 +0100

    initial commit for the AlgoFam

--- projects/AlgoAssetFreez/__test__/AlgoAssetFreez.test.ts
+++ projects/AlgoAssetFreez/__test__/AlgoAssetFreez.test.ts
@@ -1,6 +1,10 @@
 import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
 import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
 import * as algokit from '@algorandfoundation/algokit-utils';
+import {
+  algos,
+  getOrCreateKmdWalletAccount,
+} from '@algorandfoundation/algokit-utils';
 import { AlgoAssetFreezClient } from '../contracts/clients/AlgoAssetFreezClient';
 
 const fixture = algorandFixture();
@@ -11,39 +15,84 @@ let appClient: AlgoAssetFreezClient;
 describe('AlgoAssetFreez', () => {
   beforeEach(fixture.beforeEach);
 
+  let assetId: bigint|number;
+
   beforeAll(async () => {
     await fixture.beforeEach();
     const { testAccount } = fixture.context;
     const { algorand } = fixture;
 
+    await getOrCreateKmdWalletAccount(
+      { name: 'accountA', fundWith: algos(10) },
+      algorand.client.algod,
+      algorand.client.kmd
+    );
+    await getOrCreateKmdWalletAccount(
+      { name: 'accountB', fundWith: algos(10) },
+      algorand.client.algod,
+      algorand.client.kmd
+    );
+    const accountA = await algorand.account.fromKmd('accountA');
+
     appClient = new AlgoAssetFreezClient(
       {
-        sender: testAccount,
+        sender: accountA,
         resolveBy: 'id',
         id: 0,
       },
       algorand.client.algod
     );
 
+    //Creat asset
+    const assetCreated = await algorand.send.assetCreate({
+      sender: accountA.addr,
+      total: 10n, // minting total of 10 asset
+      freeze: accountA.addr,// this is saying that the given addr i.e this AccountA can freez/unfreez asset of any account using/having the token
+    });
+    assetId = assetCreated.confirmation.assetIndex!
+
     await appClient.create.createApplication({});
   });
 
-  test('sum', async () => {
-    const a = 13;
-    const b = 37;
-    const sum = await appClient.doMath({ a, b, operation: 'sum' });
-    expect(sum.return?.valueOf()).toBe(BigInt(a + b));
+  test('OptIn accountB', async () => {
+    const { algorand } = fixture;
+    const accountB = await algorand.account.fromKmd('accountB');
+    // optin accountB to the asset (assetId)
+    await algorand.send.assetOptIn({
+      assetId: BigInt(assetId),
+      sender: accountB.addr,
+    })
+    console.log("accountB opted In ")
   });
 
-  test('difference', async () => {
-    const a = 13;
-    const b = 37;
-    const diff = await appClient.doMath({ a, b, operation: 'difference' });
-    expect(diff.return?.valueOf()).toBe(BigInt(a >= b ? a - b : b - a));
-  });
+  test('SendAndFreezAsset', async () => {
+    const { algorand } = fixture;
+    const accountA = await algorand.account.fromKmd('accountA');
+    const accountB = await algorand.account.fromKmd('accountB');
 
-  test('hello', async () => {
-    const diff = await appClient.hello({ name: 'world!' });
-    expect(diff.return?.valueOf()).toBe('Hello, world!');
+    // Send asset transfer
+    await algorand.send.assetTransfer({
+      assetId: BigInt(assetId),
+      sender: accountA.addr,
+      receiver: accountB.addr,
+      amount: 1n,
+    }),
+    console.log("Asset Transfered to accountB")
+    // Freez the asset
+    await algorand.send.assetFreeze({
+      assetId: BigInt(assetId),
+      sender: accountA.addr,
+      account: accountB.addr,
+      frozen: true,
+    }),
+    console.log("Asset is frozed in accountB")
+    // check accountA asset balance
+    const { balance: accountA_Balance } = await algorand.account.getAssetInformation(accountA.addr, assetId);
+    expect(accountA_Balance).toBe(9n);
+    console.log("accountA Asset balance = "+accountA_Balance)
+    // check accountB asset balance
+    const { balance: AccountB_Balance } = await algorand.account.getAssetInformation(accountB.addr, assetId);
+    expect(AccountB_Balance).toBe(1n);
+    console.log("accountB Asset balance = "+AccountB_Balance)
   });
 });
--- projects/AlgoAssetFreez/contracts/AlgoAssetFreez.algo.ts
+++ projects/AlgoAssetFreez/contracts/AlgoAssetFreez.algo.ts
@@ -2,49 +2,6 @@ import { Contract } from '@algorandfoundation/tealscript';
 
 export class AlgoAssetFreez extends Contract {
   /**
-   * Calculates the sum of two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The sum of a and b
-   */
-  private getSum(a: uint64, b: uint64): uint64 {
-    return a + b;
-  }
-
-  /**
-   * Calculates the difference between two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The difference between a and b.
-   */
-  private getDifference(a: uint64, b: uint64): uint64 {
-    return a >= b ? a - b : b - a;
-  }
-
-  /**
-   * A method that takes two numbers and does either addition or subtraction
-   *
-   * @param a The first uint64
-   * @param b The second uint64
-   * @param operation The operation to perform. Can be either 'sum' or 'difference'
-   *
-   * @returns The result of the operation
-   */
-  doMath(a: uint64, b: uint64, operation: string): uint64 {
-    let result: uint64;
-
-    if (operation === 'sum') {
-      result = this.getSum(a, b);
-    } else if (operation === 'difference') {
-      result = this.getDifference(a, b);
-    } else throw Error('Invalid operation');
-
-    return result;
-  }
-
-  /**
    * A demonstration method used in the AlgoKit fullstack template.
    * Greets the user by name.
    *
